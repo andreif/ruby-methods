a420df49f25eb97d68dd7c8a30eb5b00b61944d4
Andrei Fokau <andrei.fokau@neutron.kth.se>
2010-10-07 14:55:02 +0200

    clean-up

--- mcnp_perf.rb
+++ mcnp_perf.rb
@@ -2,7 +2,9 @@
 
 class MCNP
   class Performance
+    
     TIMESTAMP = /(master set )?rendezvous( at)? nps =\s+(\d+)\s*(# of microtasks =\s+\d+)?\s*(.*)$/
+    
     def initialize log_path
       @filepath = log_path
       @text = IO.read log_path
@@ -11,12 +13,14 @@ class MCNP
       self.get_dumps
       self.print
     end
+    
     def print
       Kernel.puts "\n  MCNP Performance\n\n"
       self.print_info
       self.print_matlab
       Kernel.puts "\n"
     end
+    
     def print_info
       fmt = '%7.2e nps/sec/task'
       min = max = nil
@@ -36,15 +40,18 @@ class MCNP
       Kernel.puts rr
       self.write 'Cycle data', (r+rr), '_perf.txt'
     end
+    
     def write what, text, ext
       path = (ext =~ /\.m$/) ? @filepath.gsub('.','_') : @filepath
       File.open(path+ext,'w+').write text
       Kernel.puts "  %s written to %s" % [what, path+ext]
     end
+    
     def get_tasks
       @tasks = @text.scan(/master starting\s+(\d+)(.+)$/).flatten.first.to_i rescue nil
       @tasks += 1 if @text =~ /rendezvous at/ # mcnpx correction
     end
+    
     def get_timestamps
       require 'time'
       @prev_nps = 0
@@ -61,12 +68,14 @@ class MCNP
         @curr_nps = @next_nps
       end
     end
+    
     def get_dumps
       @dumps = []
       @text.split(/ dump\s+(\d+) on file/)[3..-1].each_slice(2) do |dmp|
         @dumps << [dmp.first, $5] if dmp.last =~ TIMESTAMP
       end
     end
+    
     def print_matlab
       start_time = @timestamps.first[-2]
       r = "data = [ % cycle nr., performance, time from start\n"
@@ -76,9 +85,7 @@ class MCNP
       end
       r << "];\n"
       r << "figure; plot(data(:,3)/60/60, data(:,2),'-','LineWidth',1.5); grid on; \nxlabel('Time, hours'); ylabel('Performace, nps/sec/task'); title('#{@filepath}')"
-      #File.open(@filepath.gsub('.','_')+'_perf.m','w+').write r
       self.write 'Cycle data for MATLAB', r, '_perf.m'
-      #return r
     end
   end
 end
